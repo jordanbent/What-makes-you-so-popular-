4bd85062c2fd37a2308a70e04c243f36f8383b7b
Jordan Bent <jbent@tcd.ie>
2018-11-29 17:49:19 +0000

    update

--- data.js
+++ data.js
@@ -1,49 +1,39 @@
 const octokit = require('@octokit/rest')()
-
-var logins = [];
-var ids = [];
-var locations = [];
-var followers_list = [];
-var following_list = [];
-var starred_list = [];
-var orgs_list = [];
-var repos_list = [];
-var created = [];
-var updated = [];
+//var octonode = require('cs3012-final/node_modules/octonode/lib/octonode/Client.js');
+//var client = octonode.client("3b366435a0ac5e52059f35c71f3b18431ea8bbed");
+const fs = require('fs');
 
 per_page = 100;
-var body = ''; 
 var count = 0 ;
+var bio = 0;
 
 function get(s)
 {   
+    fs.appendFileSync('./UserData.csv',"followers,following,starred,orgs,repos\n");
+    fs.appendFileSync('./FollowersData.csv',"number of, ")
     for(var no_of_pages = 0; no_of_pages < 2; no_of_pages++)
     {
-        octokit.users.list({since: no_of_pages, per_page}).then(body => 
+        octokit.users.list({since: (no_of_pages*100), per_page}).then(body => 
         {   
             for(var i = 0; i < per_page; i++)
             {
-                    logins[i] = body.data[i].login;
-                    ids[i] = body.data[i].id;
-                    locations[i] = body.data[i].location;
-                    followers_list[i] = body.data[i].followers_url; //needs more refining
-                    following_list[i] = body.data[i].following_url; //needs more refining
-                    starred_list[i] = body.data[i].starred_url;     //needs more refining
-                    orgs_list[i] = body.data[i].organizations_url;  //needs more refining
-                    repos_list[i] = body.data[i].repos_url;         //needs more refining
-                    if(body.data[i].created_at != null)
-                    created[i] = body.data[i].created_at;
-                    else
-                    created[i] = null;
-                    if(body.data[i].updated_at != null)
-                    updated[i] = body.data[i].updated_at;
-                    else
-                    updated[i] = null;
-                    count++;
+                username = body.data[i].login;
+                result_followers = octokit.users.listFollowersForUser({username, per_page});        
+                result_followering = octokit.users.listFollowingForUser({username, per_page});        
+                result_starred = octokit.activity.listReposStarredByUser({username, per_page});        
+                result_orgs = octokit.orgs.listForUser({username, per_page});  
+                result_repos = octokit.repos.listForUser({username, per_page});     
+
+                fs.appendFileSync('./UserData.csv',
+                result_followers.length + "," +
+                result_followering.length + "," +
+                result_starred.length + "," +
+                result_orgs.length + "," +
+                result_repos.length + "\n");
+
             }
-                
-            }).catch(function(e){console.log(e)});
-        }
+        }).catch(function(e){console.log(e)});
+    }
 }
 
 get(0);
