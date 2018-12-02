3328770ca712274349abb60e755d3396063711d3
Jordan Bent <jbent@tcd.ie>
2018-12-02 18:49:22 +0000

    Updated data retrieved - now in one file

--- data.js
+++ data.js
@@ -7,15 +7,11 @@ octokit.authenticate({
 })
 
 per_page = 100;
-no_of_pages = 2;
+no_of_pages = 3;
 
 async function get(s)
 {
-    fs.appendFileSync('./FollowersData.csv',"username,total\n");
-    fs.appendFileSync('./FollowingData.csv',"username,total\n");
-    fs.appendFileSync('./StarredData.csv',"username,total\n");
-    fs.appendFileSync('./OrgsData.csv',"username,total\n");
-    fs.appendFileSync('./ReposData.csv',"username,total,reponame,size,stargazers,language\n");
+    fs.appendFileSync('./UserData.csv',"username,followers,following,starred,orgs,repo_total,repo_name,repo_size,repo_stars\n");
     counter = 0;
     
     for(var k = 0; k < no_of_pages; k++)
@@ -28,22 +24,22 @@ async function get(s)
 
             var username = users.data[i].login;
             const followers = await octokit.users.listFollowersForUser({username, per_page});
-            fs.appendFileSync('./FollowersData.csv',username+","+followers.data.length+"\n");
-
             const following = await octokit.users.listFollowingForUser({username, per_page});
-            fs.appendFileSync('./FollowingData.csv',username+","+following.data.length+"\n");
-
             const starred = await octokit.activity.listReposStarredByUser({username, per_page});
-            fs.appendFileSync('./StarredData.csv',username+","+starred.data.length+"\n");
-
             const orgs = await octokit.orgs.listForUser({username, per_page});
-            fs.appendFileSync('./OrgsData.csv',username+","+orgs.data.length+"\n");
-
             const repos = await octokit.repos.listForUser({username, per_page});
-            for(var j = 0; j < repos.data.length; j++)
+
+            if(repos.data.length > 0)
             {
-                fs.appendFileSync('./ReposData.csv',username+","+repos.data.length+","+repos.data[j].name+","+repos.data[j].size+","+
-                repos.data[j].stargazers_count+","+repos.data[j].languages+"\n");
+                fs.appendFileSync('./UserData.csv',username+","+followers.data.length+","+following.data.length+","+
+                    starred.data.length+","+orgs.data.length+","+repos.data.length+","+repos.data[0].name+","+repos.data[0].size+","+
+                    repos.data[0].stargazers_count+","+repos.data[0].languages+"\n");
+            }
+
+            for(var j = 1; j < repos.data.length; j++)
+            {
+                fs.appendFileSync('./UserData.csv',","+","+","+","+","+","+repos.data[j].name+","+repos.data[j].size+","+
+                repos.data[j].stargazers_count+","+"\n");
             }
         }
     }
